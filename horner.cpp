089be7f8bfbea8a22613119dc6cbf2370d2bb7cf
zygzakmcqueenn <148342910+zygzakmcqueenn@users.noreply.github.com>
2026-03-25 08:41:24 +0100

    Update horner.cpp

--- horner.cpp
+++ horner.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int horner(int wsp[], int stopien, int x) {
-    int wynik = wsp[stopien]; 
-    for (int i = stopien - 1; i >= 0; i--) {
-        wynik = wynik * x + wsp[i];
+int horner(int wsp[], int st, int x) {
+    if (st == 0) {
+        return wsp[0];
     }
-    
-    return wynik;
+    return x * horner(wsp, st - 1, x) + wsp[st];
 }
 
-int main(){
-	int stopien;
-	int x;
-	cout << "Podaj stopien wielomianu: ";
-	cin >> stopien;
-	
-	int * wspolczynnik = new int[stopien + 1];
-	
-	for (int i = stopien; i >= 0; i--){
-		cout << "Podaj wspolczynnik stojacy przy potedze " << i << ": ";
-		cin >> wspolczynnik[i];
-	}
-	
-	cout << "Podaj argument: ";
-	cin >> x;
-	
-	int wynik = horner(wspolczynnik, stopien, x);
-	
-	cout << "W (" << x << ") = " << wynik << endl;
-	
-	delete[] wspolczynnik;
-	
-	return 0;
-	
+int main() {
+    int stopien;
+    int argument;
+
+    cout << "Podaj stopien wielomianu: ";
+    cin >> stopien;
+
+    int *wspolczynnik = new int[stopien + 1];
+
+    for (int i = stopien; i >= 0; i--) {
+        cout << "Podaj wspolczynnik stojacy przy potedze " << i << ": ";
+        cin >> wspolczynnik[stopien - i];
+    }
+
+    cout << "Podaj argument: ";
+    cin >> argument;
+
+    int wynik = horner(wspolczynnik, stopien, argument);
+
+    cout << "W( " << argument << " ) = " << wynik << endl;
+
+    delete[] wspolczynnik;
+
+    return 0;
 }
